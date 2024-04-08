0f3c9cd416ec68e1244a5031fdeb3ad4b278f811
AlnurKudaibergen <165632639+AlnurKudaibergen@users.noreply.github.com>
2024-04-08 13:28:26 +0500

    FINALLLL

--- src/Main.java
+++ src/Main.java
@@ -14,20 +14,20 @@ public class Main{
         System.out.println("""
                 Enter 0 if you want to quit the program 
                 Programs:
-                1.Find the min value of the array
-                2.Find the average value of the array
-                3:Check if number is prime
-                4.Find factorial of number n
-                5.Find the n-th of the Fibonacci sequence
+                1.Min value of the array
+                2.Average value of the array
+                3.Prime check
+                4.Factorial
+                5.Fibonacci
                 6.a^n
-                7.Reverse the sequence
+                7.Reverse
                 8.Check for consisting digits in string
                 9.Binomial coefficient 
                 10.GCD
                 """);
         while (true) {
             try {
-                System.out.println("Enter the number of the problem you want to solve (1-10):");
+                System.out.println("Enter the problem you want to solve (1-10):");
                 int task = sc.nextInt();
                 if (task == 0) {
                     break;
@@ -111,17 +111,17 @@ public class Main{
                         System.out.println("Time taken for the task:" + duration + "milliseconds");
                         break;
                     case 7:
-                        System.out.println("enter length of the array");
-                        n=sc.nextInt();
-                        for (int i = 0; i < length; i++) {
+                        System.out.println("enter length of the array,and sequence");
+                        length=sc.nextInt();
+                        numbers=new int[length];
+                        for (int i = 0; i < numbers.length; i++) {
                             numbers[i] = sc.nextInt();
 
                         }
+                        reverseOfArray(numbers,length);
                         startTime = System.nanoTime();
                         endTime = System.nanoTime();
                         duration = (endTime - startTime) / 1000000;
-                        int reversed = reverseOfArray(numbers);
-                        System.out.println("The reverse of given array is" + reversed);
                         System.out.println("Time taken for the task:" + duration + "milliseconds");
                         break;
                     case 8:
@@ -225,13 +225,13 @@ public class Main{
         }
         return a * degreeOfA(a, n - 1);
     }
-    static void reverseOfArray(int num){
-        if (num==0){
-            return ;
+    static void reverseOfArray(int[] arr,int n){
+        if (n == 0) {
+            System.out.println();
+            return;
         }
-        int value= sc.nextInt();
-        reverseOfArray(num-1);
-        System.out.print(value+" ");
+        System.out.print(arr[n - 1] + " ");
+        reverseOfArray(arr, n - 1);
     }
 
     static boolean isDigit(String s) {
